060bfc72a52ee4757ca3d983a00ed0a28b5a668c
Ato Toffah <atotoffah@gmail.com>
2023-08-23 17:38:39 +0000

    Added push function

--- monty.h
+++ monty.h
@@ -31,4 +31,5 @@ typedef struct instruction_s
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+void push(stack_t **stack, unsigned int line_number);
 #endif /* MONTY_H */
